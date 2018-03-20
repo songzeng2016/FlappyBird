87f18bb6cbb7f22c323b07a7a4f09f79b82cd266
songzeng <songzeng62@gmail.com>
2018-03-20 23:43:17 +0800

    完善代码

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ import {ResourceLoader} from "./js/base/ResourceLoader.js";
 import {BackGround} from "./js/runtime/BackGround.js";
 import {DataStore} from "./js/base/DataStore.js";
 import {Director} from "./js/Director.js";
+import {Land} from "./js/runtime/Land.js";
 
 export class Main {
 
@@ -10,6 +11,7 @@ export class Main {
         this.canvas = document.getElementById('game_canvas');
         this.ctx = this.canvas.getContext('2d');
         this.dataStore = DataStore.getInstance();
+        this.director = Director.getInstance();
         const loader = ResourceLoader.create();
         loader.onLoaded(map => this.onResourceFirstLoaded(map));
 
@@ -24,9 +26,13 @@ export class Main {
 
     init() {
         this.dataStore
-            .put('background', BackGround);
+            .put('pencils', [])
+            .put('background', BackGround)
+            .put('land', Land);
 
-        Director.getInstance().run();
+        // 创建铅笔要在游戏逻辑运行之前
+        this.director.createPencil();
+        this.director.run();
     }
 
 }
--- js/Director.js
+++ js/Director.js
@@ -1,5 +1,7 @@
 // 导演类，控制游戏的逻辑
 import {DataStore} from "./base/DataStore.js";
+import {UpPencil} from "./runtime/UpPencil.js";
+import {DownPencil} from "./runtime/DownPencil.js";
 
 export class Director {
 
@@ -13,11 +15,40 @@ export class Director {
 
     constructor() {
         this.dataStore = DataStore.getInstance();
+        this.moveSpeed = 2;
+    }
+
+    createPencil() {
+        const minTop = window.innerHeight / 8;
+        const maxTop = window.innerHeight / 2;
+        const top = minTop + Math.random() * (maxTop - minTop);
+        this.dataStore.get('pencils').push(new UpPencil(top));
+        this.dataStore.get('pencils').push(new DownPencil(top));
     }
 
     run() {
-        const backgroundSprite = this.dataStore.get('background');
-        backgroundSprite.draw();
+        this.dataStore.get('background').draw();
+
+        const pencils = this.dataStore.get('pencils');
+        if (pencils[0].x + pencils[0].width <= 0
+            && pencils.length === 4) {
+            pencils.shift();
+            pencils.shift();
+        }
+
+        if (pencils[0].x < (window.innerWidth - pencils[0].width) / 2
+            && pencils.length === 2) {
+            this.createPencil();
+        }
+
+        this.dataStore.get('pencils').forEach(function (value) {
+            value.draw();
+        });
+        this.dataStore.get('land').draw();
+
+        let timer = requestAnimationFrame(() => this.run());
+        this.dataStore.put('timer', timer);
+        // cancelAnimationFrame(this.dataStore.get('timer'));
     }
 
 }
