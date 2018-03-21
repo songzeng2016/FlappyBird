c71887638712c94fb45f400361c52c1e4ea5f3c5
songzeng <songzeng62@gmail.com>
2018-03-21 23:09:54 +0800

    完善项目

--- Main.js
+++ Main.js
@@ -4,6 +4,8 @@ import {BackGround} from "./js/runtime/BackGround.js";
 import {DataStore} from "./js/base/DataStore.js";
 import {Director} from "./js/Director.js";
 import {Land} from "./js/runtime/Land.js";
+import {Birds} from "./js/player/Birds.js";
+import {StartButton} from "./js/player/StartButton.js";
 
 export class Main {
 
@@ -25,14 +27,34 @@ export class Main {
     }
 
     init() {
+
+        // 首先重置游戏是没有结束的
+        this.director.isGameOver = false;
+
         this.dataStore
             .put('pencils', [])
             .put('background', BackGround)
-            .put('land', Land);
+            .put('land', Land)
+            .put('birds', Birds)
+            .put('startButton', StartButton);
 
+        this.registerEvent();
         // 创建铅笔要在游戏逻辑运行之前
         this.director.createPencil();
         this.director.run();
+    }
+
+    registerEvent() {
+        this.canvas.addEventListener('click', e => {
+            // 阻止事件冒泡
+            e.preventDefault();
+            if (this.director.isGameOver) {
+                console.log('游戏开始');
+                this.init();
+            } else {
+                this.director.birdsEvent();
+            }
+        });
     }
 
 }
--- js/Director.js
+++ js/Director.js
@@ -26,29 +26,98 @@ export class Director {
         this.dataStore.get('pencils').push(new DownPencil(top));
     }
 
-    run() {
-        this.dataStore.get('background').draw();
+    birdsEvent() {
+        for (let i = 0; i <= 2; i++) {
+            this.dataStore.get('birds').y[i] =
+                this.dataStore.get('birds').birdsY[i];
+        }
+        this.dataStore.get('birds').time = 0;
+    }
 
+    // 判断小鸟是否和铅笔撞击
+    static isStrike(bird, pencil) {
+        let s = false;
+        if (bird.top > pencil.bottom
+            || bird.bottom < pencil.top
+            || bird.right < pencil.left
+            || bird.left > pencil.right) {
+            s = true;
+        }
+        return !s;
+    }
+
+    // 判断小鸟是否撞击地饭和铅笔
+    check() {
+        const birds = this.dataStore.get('birds');
+        const land = this.dataStore.get('land');
         const pencils = this.dataStore.get('pencils');
-        if (pencils[0].x + pencils[0].width <= 0
-            && pencils.length === 4) {
-            pencils.shift();
-            pencils.shift();
+
+        if (birds.birdsY[0] + birds.birdsHeight[0] > land.y) {
+            this.isGameOver = true;
+            return;
         }
 
-        if (pencils[0].x < (window.innerWidth - pencils[0].width) / 2
-            && pencils.length === 2) {
-            this.createPencil();
+        // 小鸟的模型
+        const birdsBorder = {
+            top: birds.y[0],
+            bottom: birds.y[0] + birds.birdsHeight[0],
+            left: birds.birdsX[0],
+            right: birds.birdsX[0] + birds.birdsWidth[0]
+        };
+
+        const length = pencils.length;
+        for (let i = 0; i < length; i++) {
+            const pencil = pencils[i];
+            const pencilBorder = {
+                top: pencil.y,
+                bottom: pencil.y + pencil.height,
+                left: pencil.x,
+                right: pencil.x + pencil.width
+            };
+
+            if (Director.isStrike(birdsBorder, pencilBorder)) {
+                console.log('撞到铅笔了');
+                this.isGameOver = true;
+                return;
+            }
         }
 
-        this.dataStore.get('pencils').forEach(function (value) {
-            value.draw();
-        });
-        this.dataStore.get('land').draw();
+    }
 
-        let timer = requestAnimationFrame(() => this.run());
-        this.dataStore.put('timer', timer);
-        // cancelAnimationFrame(this.dataStore.get('timer'));
+
+    run() {
+        this.check();
+        if (!this.isGameOver) {
+            this.dataStore.get('background').draw();
+
+            // 铅笔走到左边移出屏幕则消除铅笔
+            const pencils = this.dataStore.get('pencils');
+            if (pencils[0].x + pencils[0].width <= 0
+                && pencils.length === 4) {
+                pencils.shift();
+                pencils.shift();
+            }
+
+            // 第一组铅笔走到屏幕中央且只有一组的时候再创建一组铅笔
+            if (pencils[0].x < (window.innerWidth - pencils[0].width) / 2
+                && pencils.length === 2) {
+                this.createPencil();
+            }
+
+            this.dataStore.get('pencils').forEach(function (value) {
+                value.draw();
+            });
+            this.dataStore.get('land').draw();
+            this.dataStore.get('birds').draw();
+
+            let timer = requestAnimationFrame(() => this.run());
+            this.dataStore.put('timer', timer);
+        } else {
+            console.log('游戏结束');
+            this.dataStore.get('startButton').draw();
+            cancelAnimationFrame(this.dataStore.get('timer'));
+            this.dataStore.destroy();
+        }
     }
 
 }
